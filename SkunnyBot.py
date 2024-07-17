7fd20a909a337a0b5cacbbf473c25402996e0725
Production1O1 <juicyyellow123456@gmail.com>
2024-07-17 12:36:02 -0500

    Update SkunnyBot.py

--- SkunnyBot.py
+++ SkunnyBot.py
@@ -102,3 +102,46 @@ async def version(ctx):
     embed = discord.Embed(
         colour = discord.Colour.blue()
     )
+   
+    
+    embed.set_author(name='**Update log**!')
+    embed.add_field(name='1.81', value='Added +daily and +lbg minor bug fixes and patches.', inline=False)
+    embed.add_field(name='1.8', value=' Added economy system, ticket system, Ai chatbot, 2 more commands and bug fixes since update 1.3!')
+    embed.add_field(name='1.7', value=' Added 20 more commands and a new error handler for +kick. Cooldown on commands also added!')
+    embed.add_field(name='1.6', value=' Reramped help command and added embeds to some commands')
+    embed.add_field(name='1.5', value=' Made the bot run 24/7!')
+    embed.add_field(name='1.4', value=' Just added 10 commands and specific command reactions')
+    embed.add_field(name='1.3', value=' Fixed a major bug in most moderation commands and added some other stuff to the bot')
+    embed.add_field(name='1.2', value=' Added Moderation commands')
+    embed.add_field(name='1.1', value=' **nothing added idk why i even called it a update**')
+    embed.add_field(name='1.0', value=' Bot got realeased into the public')
+    embed.add_field(name='0.1', value=' Added one command. Just **one**')
+    embed.add_field(name='0.5', value=' Added like 20 commands.')
+    await ctx.send(embed = embed)
+
+
+    print("context",ctx)
+#dbl = disbotlist(bot,"secret")
+@bot.command()
+async def botserver(ctx):
+  await ctx.send(f"I'm in {len(bot.guilds)} servers!")
+#@bot.event
+#async def on_command_error(ctx, error):
+ # if isinstance(error, commands.CommandOnCooldown):
+  #  msg = '**Command is still on cooldown**, try again in {:.2f} hours'.format(error.retry_after)
+
+
+@bot.command()
+@commands.has_permissions(administrator = True)
+async def changeprefix(ctx, prefix):
+
+    with open("prefixes.json", "r") as f:
+        prefixes = json.load(f)
+
+    prefixes[str(ctx.guild.id)] = prefix
+
+    with open("prefixes.json", "w") as f:
+        json.dump(prefixes,f)    
+
+    await ctx.send(f"The prefix was changed to {prefix}")
+
