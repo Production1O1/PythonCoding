c6017d9f8024fd6e4a9844167e7db84887bb1a20
Production1O1 <juicyyellow123456@gmail.com>
2024-07-18 12:30:40 -0500

    Update SkunnyBot.py

--- SkunnyBot.py
+++ SkunnyBot.py
@@ -591,3 +591,45 @@ winningConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ]
+@bot.command()
+async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
+    global count
+    global player1
+    global player2
+    global turn
+    global gameOver
+
+    if gameOver:
+        global board
+        board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+        turn = ""
+        gameOver = False
+        count = 0
+
+        player1 = p1
+        player2 = p2
+
+        # print the board
+        line = ""
+        for x in range(len(board)):
+            if x == 2 or x == 5 or x == 8:
+                line += " " + board[x]
+                await ctx.send(line)
+                line = ""
+            else:
+                line += " " + board[x]
+
+        # determine who goes first
+        num = random.randint(1, 2)
+        if num == 1:
+            turn = player1
+            await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
+        elif num == 2:
+            turn = player2
+            await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
+    else:
+        em = discord.Embed(title='A game is already in progress! Finish it or end the current game using +end before starting a new one',color=discord.Color.random())
+        em.set_footer(text="a game was already running")
+        await ctx.send(embed=em)
